b9796cf0e5a48b080347a41b053bf7f79836a14a
potter <38352096+enjoypython@users.noreply.github.com>
2021-08-19 19:49:12 +0900

    Create 04_factory-pattern.py

---
+++ 04_factory-pattern.py
@@ -0,0 +1,67 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Pokemon(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def name(self):
+        pass
+
+    @property
+    @abstractmethod
+    def paint(self):
+        pass
+
+    def __str__(self):
+        return self.paint + " " + self.name
+
+
+class Pikachu(Pokemon):
+    @property
+    def name(self):
+        return "Pikachu"
+
+    @property
+    def paint(self):
+        return "Yellow"
+
+
+class Squirtle(Pokemon):
+    @property
+    def name(self):
+        return "Squirtle"
+
+    @property
+    def paint(self):
+        return "Blue"
+
+
+class Factory(metaclass=ABCMeta):
+    @abstractmethod
+    def create_pokemon(self, type):
+        pass
+
+
+class ElectronicPokemonFactory(Factory):
+    def create_pokemon(self, type):
+        if type == "Pikachu":
+            print("Created", type, "Pokemon!")
+            return Pikachu()
+
+
+class WaterPokemonFactory(Factory):
+    def create_pokemon(self, type):
+        if type == "Squirtle":
+            print("Created", type, "Pokemon!")
+            return Squirtle()
+
+
+electro_pokemon_factory = ElectronicPokemonFactory()
+pikachu = electro_pokemon_factory.create_pokemon("Pikachu")
+print(pikachu)
+
+print()
+
+water_pokemon_factory = WaterPokemonFactory()
+squirtle = water_pokemon_factory.create_pokemon("Squirtle")
+print(squirtle)
