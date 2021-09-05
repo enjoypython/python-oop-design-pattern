e97345a4a12fac34111b5744a563e5b9ba4791be
potter <thkang.korea@gmail.com>
2021-09-05 11:00:21 +0900

    Create 12_iterator-pattern.py

---
+++ 12_iterator-pattern.py
@@ -0,0 +1,49 @@
+from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass
+class Pokemon(metaclass=ABCMeta):
+    index: int
+    name: str
+
+    def method(self):
+        print(f"{self.name} has been called!")
+
+
+class Iterator(metaclass=ABCMeta):
+    @staticmethod
+    @abstractmethod
+    def has_next():
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def next():
+        pass
+
+
+@dataclass
+class PokemonIterator(Iterator):
+    aggregates: list
+    index: int = 0
+
+    def has_next(self):
+        return self.index < len(self.aggregates)
+
+    def next(self):
+        if self.index < len(self.aggregates):
+            aggregate = self.aggregates[self.index]
+            self.index += 1
+            return aggregate
+        raise Exception("End of Iterator")
+
+
+bulbasaur = Pokemon(1, "bulbasaur")
+charmander = Pokemon(2, "charmander")
+squirtle = Pokemon(3, "squirtle")
+
+pokemon_aggregates = [bulbasaur, charmander, squirtle]
+iterable = PokemonIterator(pokemon_aggregates)
+while iterable.has_next():
+    iterable.next().method()
